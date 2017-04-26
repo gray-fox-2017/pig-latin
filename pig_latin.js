f739953af428ca32b5b34f7d85ba4f1137e5a867
daniarmeiradi <mailtoreidy@gmail.com>
2017-04-26 17:58:55 +0700

    sudah selesai + dengan prompt

--- pig_latin.js
+++ pig_latin.js
@@ -2,7 +2,41 @@
 
 //use readline to fix this challenge
 const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+rl.setPrompt('Masukkan kata!>')
+rl.prompt();
+rl.on('line', (line) => {
+  pigLatin(line);
+ var akhir = pigLatin(line);
+  console.log(`Sandi Babi: ${akhir}`);
+  rl.prompt();
+}).on('close', () => {
+  console.log('Have a great day!');
+  process.exit(0);
+});
 
 function pigLatin(sentence) {
   // Your pig latin implementation here...
+  var vowel = ['a', 'e', 'i', 'e', 'o'];
+  var word = sentence.split(' ');
+
+  for (let i=0;i<word.length;i++) {
+    if (/^[aiueo]/gi.test(word[i])) {
+
+    } else if (/[aiueo]/gi.test(word[i]) === false) {
+       var baru = word[i].concat('ay');
+       word[i] = baru
+    } else if (/[aiueo]/gi.test(word[i])) {
+      var hasil = word[i].match(/[aiueo]/i);
+      var awal = word[i].substring(hasil.index, (word[i].length));
+      var akhir = word[i].substring(0, (hasil.index)) + 'ay';
+      var baru = awal.concat(akhir);
+      word[i] = baru;
+    }
+  }
+
+  return word.join(' ');
 }

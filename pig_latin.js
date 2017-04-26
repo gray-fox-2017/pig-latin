8c381a3448d51d439f71c65c058ecc7d9122e677
ppsari <poppymighty@gmail.com>
2017-04-26 14:52:17 +0700

    p1w1d2_2_piglatin

--- pig_latin.js
+++ pig_latin.js
@@ -2,7 +2,44 @@
 
 //use readline to fix this challenge
 const readline = require('readline');
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-function pigLatin(sentence) {
-  // Your pig latin implementation here...
+rl.question("Convert to pig-latin: ", function(answer) {
+  // TODO: Log the answer in a database
+  console.log("Thank you for your valuable feedback:", answer);
+
+  rl.close();
+  console.log('Hasil : '+convertAll(answer));
+
+});
+
+function convertAll(sentence){
+  let res = [];
+  let arrSen = sentence.split(' ');
+  let len = arrSen.length;
+
+  arrSen.forEach((str)=>{
+    res.push(pigLatin(str));
+  });
+  return 'Perubahan : '+res.join(' ')+' ; Kata yang berubah: '+len;
+}
+
+function pigLatin(str) {
+  let arrStr = str.split('');
+  let len = arrStr.length;
+  let itemp = 0;
+
+  for (let i = 0 ; i < len; i++) {
+    let tchar = str[i];
+    if (tchar.search(/[aiueo]/i) === -1) {
+      arrStr.splice(itemp,1);
+      arrStr.push(tchar+'ay');
+    } else {
+      itemp += 1;
+    }
+  }
+  return arrStr.join('');
 }

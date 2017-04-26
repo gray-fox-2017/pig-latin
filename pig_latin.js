c727ed4765c7531cace4519df84f8fa3ad451cfe
hakiemaul <noersedya@gmail.com>
2017-04-26 15:35:50 +0700

    release 0 completed

--- pig_latin.js
+++ pig_latin.js
@@ -3,6 +3,26 @@
 //use readline to fix this challenge
 const readline = require('readline');
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let sentence='';
+rl.question('Type a word! ', (answer) => {
+  sentence=answer;
+  console.log(pigLatin(sentence));
+});
+rl.on('close', function() {
+  console.log('Bye!');
+  process.exit(0);
+});
+
 function pigLatin(sentence) {
-  // Your pig latin implementation here...
+  if(/^[aiueo]\w+/g.test(sentence)) return sentence;
+  let vowel=sentence.match(/[aiueo]\w+/g);
+  let cons=sentence.replace(vowel,'')
+  return vowel+cons+'ay';
 }
+
+// pigLatin('tes')

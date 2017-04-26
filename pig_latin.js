d2655a341aa3088bf0c1049a325072022ce14b61
ingelieur <ingelieur@gmail.com>
2017-04-26 15:25:15 +0700

    Almost

--- pig_latin.js
+++ pig_latin.js
@@ -3,6 +3,35 @@
 //use readline to fix this challenge
 const readline = require('readline');
 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Please insert the word that you\'d like to be pig-latinized: ', (answer) => {
+  console.log('The pig latinized word is: ', pigLatin(answer));
+  // console.log(`${answer} is going to be pig latinized.`);
+});
+
+rl.on('close', function() {
+    console.log('Thanks for playing!');
+    process.exit(0);
+});
+
 function pigLatin(sentence) {
-  // Your pig latin implementation here...
+  let isThereVowel = [];
+  let pigLatinized = "";
+  isThereVowel.push(sentence.match(/[aiueo]/i).index);
+  console.log(isThereVowel);
+  if (isThereVowel.length > 0) {
+    let indexVowel = sentence.match(/[aiueo]/i).index;
+    pigLatinized = sentence.substring(indexVowel) + sentence.substring(0,indexVowel) + "ay";
+  }
+  else {
+    pigLatinized = sentence + "ay";
+  } 
+  return pigLatinized
 }
+
+
+

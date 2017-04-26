e7ababa18e05a3ac7aa576af28a6c12c74d89337
erwar <erwinwahyuramadhan@gmail.com>
2017-04-26 18:36:36 +0700

    pig latin using prompt and readline

--- pig_latin.js
+++ pig_latin.js
@@ -3,6 +3,34 @@
 //use readline to fix this challenge
 const readline = require('readline');
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: "Input Kata : " //prompt inisialisasi yang dimunculkan awal kali program berjalan
+});
+
+rl.prompt(); //memunculkan prompt
+rl.on('line', (sentence) => {
+  sentence = sentence.split(' ');
+  console.log(pigLatin(sentence)); //memanggil function pigLatin
+  rl.prompt(); //memunculkan kemmbali prompt
+});
+
 function pigLatin(sentence) {
   // Your pig latin implementation here...
+  let vowels = /[aiueo]/i;
+  let konsonan = /[^aiueo]*/i;
+  let hasil = [];
+  for (let i = 0; i <sentence.length; i++) {
+    if (vowels.test(sentence[i][0]) == true) {
+      hasil.push(sentence[i]);
+    } else {
+      let toVowels = sentence[i].replace(konsonan, ""); //menjadikan konsonan menjadi vowels dengan mengganti nilai konsonan dengan str blank ""
+      let toKonsonan = sentence[i].match(konsonan); // me match nilai konsonan
+
+      hasil.push(toVowels + toKonsonan + 'ay');
+    }
+
+  }
+  return hasil.join(' ')
 }

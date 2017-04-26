77edcd9eb7af0c8a46ecaf815e41213b74b25202
Priambodo-Kurniawan <iam@calibreworks.co.id>
2017-04-26 16:38:32 +0700

    Pig Latin done -iam

--- pig_latin.js
+++ pig_latin.js
@@ -4,5 +4,34 @@
 const readline = require('readline');
 
 function pigLatin(sentence) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  rl.setPrompt('Masukan input: ');
+  rl.prompt();
+  rl.on('line', (line) => {
+    console.log(`Kata Anda: ${line}`);
+    var arr = line.split(' ');
+    var newWord = [];
+    for(var i=0; i<arr.length; i++){
+      if (/^[aeiou]/.test(arr[i])){
+        newWord.push(arr[i]);
+      } else {
+        var consonants = arr[i].match(/.+?(?=[aiueo])/)[0];
+        var word = arr[i].match(/[aiueo].*$/)[0] + consonants + 'ay.';
+        newWord.push(word);
+      }
+    }
+    var joinWord = newWord.join(' ');
+    console.log(joinWord);
+    rl.prompt();
+  }).on('close', ()=> {
+    console.log('Have a great day!');
+    process.exit(0);
+  })
   // Your pig latin implementation here...
 }
+
+pigLatin();
